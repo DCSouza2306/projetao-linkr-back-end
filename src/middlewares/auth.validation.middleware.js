c39f2f8301a881a41962f3925cfe41d9abc46199
Diego Candido <diegocan.sou@gmail.com>
2023-01-04 18:39:46 -0300

    feat: authValidation

---
+++ src/middlewares/auth.validation.middleware.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+import { findUserById } from "../repositories/users.repository.js";
+
+export async function authValidation(req, res, next) {
+  try {
+    const { authorization } = req.headers;
+
+    if (!authorization) return res.sendStatus(401);
+
+    const parts = authorization.split(" ");
+
+    if (parts.length !== 2) return res.sendStatus(401);
+
+    const [schema, token] = parts;
+
+    if (schema !== "Bearer") return res.sendStatus(401);
+
+    jwt.verify(token, process.env.SECRET_JWT, async (error, decoded) => {
+      if (error) {
+        return res.status(401).send({ message: "Invalid or expired token" });
+      }
+
+      const { rows } = await findUserById(decoded.id);
+
+      if (!rows[0]) {
+        res.status(404).send({ message: "Can not find user" });
+      }
+
+      res.locals.userId = rows[0].id;
+
+      return next();
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e);
+  }
+}
