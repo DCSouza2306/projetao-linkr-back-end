6a096caa068f240627750634d8729ed8945cd272
Diego Candido <diegocan.sou@gmail.com>
2023-01-04 18:46:22 -0300

    fix: AuthValidation

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,14 +3,11 @@ import { userSchema, loginSchema } from "../models/Users.js";
 import { createUser } from "../controllers/users.controller.js";
 import { validateSchema } from "../middlewares/validate.schema.middleware.js";
 import { signIn } from "../controllers/auth.controller.js";
-import { authValidation } from "../middlewares/auth.validation.middleware.js";
 
 const router = Router();
 
 router.post("/sign-up", validateSchema(userSchema), createUser);
 
 router.post("/", validateSchema(loginSchema), signIn);
-
-router.get("/teste", authValidation)
 
 export default router;
