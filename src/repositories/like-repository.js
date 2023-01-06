6c11928360f99c8a2fe84129c13012fe0496765a
Emerson <emersonkilon@gmail.com>
2023-01-06 13:47:49 -0300

    feat: routes like, unlike, numberOfAllLikes, isLiked ready

---
+++ src/repositories/like-repository.js
@@ -0,0 +1,18 @@
+import db from "../database/db.js";
+
+
+export function postLike({userId, postId}){
+    return db.query(`INSERT INTO likes ("user-id","post-id") VALUES ($1,$2)`,[userId, postId])
+}
+
+export function deleteLike({userId, postId}){
+    return db.query(`DELETE FROM likes WHERE "user-id"=$1 AND "post-id"=$2`,[userId, postId])
+}
+
+export function getLikes({postId}){
+    return db.query(`SELECT COUNT(*) as "totalLikes" FROM likes WHERE "post-id"=($1)`,[postId])
+}
+
+export function like({userId, postId}){
+    return db.query(`SELECT * FROM likes WHERE "user-id"=$1 AND "post-id"=$2`,[userId, postId])
+}
