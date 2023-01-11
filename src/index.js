de35207b8a238248955dfcd2fae9cce21a351837
Diego Candido <diegocan.sou@gmail.com>
2023-01-11 18:31:46 -0300

    chore: merge try

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import indexRoutes from "./routes/index.routes.js";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-dotenv.config();
 app.use(indexRoutes);
 
 const port = process.env.PORT || 4000;
