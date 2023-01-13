9584fbcbfd4ee10bd7a61b47ac1f7eb249db6a7f
Diego Candido <diegocan.sou@gmail.com>
2023-01-13 13:09:39 -0300

    refact: users-repository

--- src/repositories/users-page-repository.js
+++ src/repositories/users-page-repository.js
@@ -2,16 +2,28 @@ import db from "../database/db.js";
 export async function get_user_page_data(id, req, res) {
   try {
     const posts = await db.query(
-      ` SELECT users.id, users.name, users."url-image" AS "url_image", posts.id AS "post_id", posts."user-id" AS "user_id", posts.link, posts.content 
-      FROM users JOIN posts ON users.id = posts."user-id" WHERE users.id=$1`,
-      [id]
+      ` SELECT posts.id, posts.link, posts.content, users.id AS "userId", users.name, users."url-image" AS "urlImage",
+      posts."meta-title" AS "metaTitle", posts."meta-description" AS "metaDesc", posts."meta-image" AS "metaImage"
+      FROM posts
+      JOIN users
+      ON posts."user-id" = users.id
+      WHERE users.id = $1
+      ORDER BY posts.id DESC
+      LIMIT 20
+      ;`, [id]
     );
 
     if (posts.rowCount === 0) {
       const user = await db.query(
-        `
- SELECT users.name, users."url-image" AS "url_image" FROM users WHERE id=$1`,
-        [id]
+        ` SELECT posts.id, posts.link, posts.content, users.id AS "userId", users.name, users."url-image" AS "urlImage",
+        posts."meta-title" AS "metaTitle", posts."meta-description" AS "metaDesc", posts."meta-image" AS "metaImage"
+        FROM posts
+        JOIN users
+        ON posts."user-id" = users.id
+        WHERE users.id = $1
+        ORDER BY posts.id DESC
+        LIMIT 20
+        ;`, [id]
       );
       if (user.rowCount === 0) {
         return "User not found";
